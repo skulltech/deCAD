b3df4d6be15d7970fa0e9d6628904544eed2642c
Ankit Solanki <ankit03june@gmail.com>
2018-03-05 00:31:18 +0530

    Add files via upload

---
+++ cop290ass3.cpp
@@ -0,0 +1,28 @@
+#include <stdio>
+#include <math>
+#include <QPainter>
+/*
+Class from QT module whichever needed
+*/
+
+using namespace std;
+
+/**
+* This class will be used to interact with the QPainter library to
+* provide the user a drawing pad for drawing shapes to input.
+*/
+class drawingPad2D {
+
+};
+
+
+/**
+*
+*/
+int algorithm2Dto3D();
+
+
+/**
+*
+*/
+int algorithm3Dto2D();
