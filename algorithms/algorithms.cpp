f32c5aed6b1f476f59b088f47b8a3c44e3bcafcf
Ankit Solanki <ankit03june@gmail.com>
2018-03-10 22:55:08 +0530

    working 2D-3D algo for 1-vertex

--- algorithms/algorithms.cpp
+++ algorithms/algorithms.cpp
@@ -1,33 +1,18 @@
 #include <iostream>
 #include <set>
 #include <tuple>
-#include <edge.h>
 
 using namespace std;
 
-class edge{
-
-	public:
-	float length;
-	tuple <vertex,vertex> e;
-	
-	void makeEdge(vertex v1, vertex v2){
-		e = make_tuple (v1,v2);
-	}
-
-	void printEdge(){
-		cout<<get<0>(get<0>(e))-get<0>(get<1>(e))<<"i^ + "<<get<1>(get<0>(e))-get<1>(get<1>(e))<<"j^ +"<<get<2>(get<0>(e))-get<2>(get<1>(e))<<"k^ "<<endl;
-	}
-
-};
-
 class vertex{
+	
 	public:
 	tuple <float,float,float> v;
 
 	vertex(float a, float b, float c){
 		v = make_tuple(a,b,c);
 	}
+	
 	void printVertex(){
 		cout<<(get<0>(v))<<", "<<(get<1>(v))<<", "<<(get<2>(v))<<endl;
 	}
@@ -40,10 +25,11 @@ class plane {
 
 	plane(float a1,float b1, float c1, float d1){
 		a=a1;b=b1;c=c1;d=d1;
+		normal = make_tuple(a1,b1,c1);
 	}
 
 	void printNormal(){
-		cout<<get<0>(get<0>(normal))-get<0>(get<1>(normal))<<"i^ + "<<get<1>(get<0>(normal))-get<1>(get<1>(normal))<<"j^ +"<<get<2>(get<0>(normal))-get<2>(get<1>(normal))<<"k^ "<<endl;
+		cout<<(get<0>(normal))<<"i^ + "<<get<1>(normal)<<"j^ +"<<get<2>(normal)<<"k^ "<<endl;
 	}
 
 	float squareNormal(){
@@ -53,18 +39,18 @@ class plane {
 
 class algorithms {
     
-	protected:
+	public:
 	/**
 	* Algorithm implementation for converting given 2D projections
 	* into a 3D model.
 	*/
-	vertex algorithm2Dto3D(vertex v, edge e, plane p){
+	vertex algorithm2Dto3D(vertex v, plane p){
 
-		int temp1=(get<0>(p.getNormal()))*(get<0>(v.v))+(get<1>(p.getNormal()))*(get<1>(v.v))+(get<2>(p.getNormal()))*(get<2>(v.v));
-		int temp2=(p.squareNormal())
+		float temp1=(get<0>(p.normal)*(get<0>(v.v))+(get<1>(p.normal)*(get<1>(v.v)))+(get<2>(p.normal))*(get<2>(v.v)));
+		float temp2=(p.squareNormal());
 
-		vertex((temp1/temp2)*(get<0>(p.normal)),(temp1/temp2)*(get<1>(p.normal)),(temp1/temp2)*(get<2>(p.normal))) projw;
-		vertex((get<0>(v.v)-get<0>(projw.v)),(get<1>(v.v)-get<1>(projw.v)),(get<2>(v.v)-get<2>(projw.v))) q;
+		vertex projw((temp1/temp2)*(get<0>(p.normal)),(temp1/temp2)*(get<1>(p.normal)),(temp1/temp2)*(get<2>(p.normal)));
+		vertex q((get<0>(v.v)-get<0>(projw.v)),(get<1>(v.v)-get<1>(projw.v)),(get<2>(v.v)-get<2>(projw.v)));
 
 		return q;				
 	}
@@ -80,14 +66,13 @@ class algorithms {
 
 int main(){
 
-	edge e1; //will be a set
-	vertex v1; //will be a set
-	plane p1;
-
+	vertex v1(2,2,2); //will be a set
+	plane p1(1,2,3,4);
+	v1.printVertex();
+	p1.printNormal();
 	algorithms a1;
-	vertex res = a1.algorithm2Dto3D(v1, e1, p1);
-
-	cout<<"resultant vertex is"<< 
+	vertex res = a1.algorithm2Dto3D(v1, p1);
+	cout<<(get<0>(res.v));
 
 	return 0;
 }
--- algorithms/testing.cpp
+++ algorithms/testing.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <set>
 #include <tuple>
+#include <edge.h>
 
 using namespace std;
+
+class edge{
+
+	public:
+	float length;
+	tuple <vertex,vertex> e;
+	
+	void makeEdge(vertex v1, vertex v2){
+		e = make_tuple (v1,v2);
+	}
+
+	void printEdge(){
+		cout<<get<0>(get<0>(e))-get<0>(get<1>(e))<<"i^ + "<<get<1>(get<0>(e))-get<1>(get<1>(e))<<"j^ +"<<get<2>(get<0>(e))-get<2>(get<1>(e))<<"k^ "<<endl;
+	}
+
+};
 
 class vertex{
 	public:
@@ -16,24 +33,6 @@ class vertex{
 	}
 };
 
-
-class edge{
-
-	public:
-	float length;
-	tuple<vertex,vertex> e;
-	
-	// edge(vertex v1, vertex v2){
-	// 	e = make_tuple(v1,v2);
-	// }
-
-	// void printEdge(){
-	// 	cout<<(get<0>(get<0>(e)))-(get<0>(get<1>(e)))<<"i^ + "<<get<1>(get<0>(e))-get<1>(get<1>(e))<<"j^ +"<<get<2>(get<0>(e))-get<2>(get<1>(e))<<"k^ "<<endl;
-	// }
-
-};
-
-
 class plane {
 	public:
 	float a,b,c,d; //the equation of plane is ax+by+cz+d=0;
@@ -41,11 +40,10 @@ class plane {
 
 	plane(float a1,float b1, float c1, float d1){
 		a=a1;b=b1;c=c1;d=d1;
-		normal = make_tuple(a1,b1,c1);
 	}
 
 	void printNormal(){
-		cout<<(get<0>(normal))<<"i^ + "<<(get<1>(normal))<<"j^ +"<<(get<2>(normal))<<"k^ "<<endl;
+		cout<<get<0>(get<0>(normal))-get<0>(get<1>(normal))<<"i^ + "<<get<1>(get<0>(normal))-get<1>(get<1>(normal))<<"j^ +"<<get<2>(get<0>(normal))-get<2>(get<1>(normal))<<"k^ "<<endl;
 	}
 
 	float squareNormal(){
@@ -53,11 +51,43 @@ class plane {
 	}
 };
 
+class algorithms {
+    
+	protected:
+	/**
+	* Algorithm implementation for converting given 2D projections
+	* into a 3D model.
+	*/
+	vertex algorithm2Dto3D(vertex v, edge e, plane p){
+
+		int temp1=(get<0>(p.getNormal()))*(get<0>(v.v))+(get<1>(p.getNormal()))*(get<1>(v.v))+(get<2>(p.getNormal()))*(get<2>(v.v));
+		int temp2=(p.squareNormal())
+
+		vertex((temp1/temp2)*(get<0>(p.normal)),(temp1/temp2)*(get<1>(p.normal)),(temp1/temp2)*(get<2>(p.normal))) projw;
+		vertex((get<0>(v.v)-get<0>(projw.v)),(get<1>(v.v)-get<1>(projw.v)),(get<2>(v.v)-get<2>(projw.v))) q;
+
+		return q;				
+	}
+
+	/**
+	* Algorithm implementation for converting given 3D model 
+	* into a 2D projection on the given plane.
+	*/
+	void algorithm3Dto2D(){
+
+	}
+};
 
 int main(){
-	cout<<"done"<<endl;
-	vertex v2(1,2,3.14);
-	vertex v1(2,2.71,3);
+
+	edge e1; //will be a set
+	vertex v1; //will be a set
+	plane p1;
+
+	algorithms a1;
+	vertex res = a1.algorithm2Dto3D(v1, e1, p1);
+
+	cout<<"resultant vertex is";
 
 	return 0;
 }
