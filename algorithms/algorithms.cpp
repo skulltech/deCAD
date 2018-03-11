733618e382a6f4110c8d2ef99386a83e7def691e
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-03-11 22:36:58 +0530

    Add algorithm for model to projections

--- algorithms/algorithms.cpp
+++ algorithms/algorithms.cpp
@@ -19,6 +19,20 @@ class vertex {
 };
 
 
+class point {
+	public:
+	tuple <float,float> p;
+
+	point(float a, float b) {
+		p = make_tuple(a, b);
+	}
+	
+	void print_vertex() {
+		cout << (get<0>(p)) << ", " << (get<1>(p)) << endl;
+	}
+};
+
+
 class plane {
 	public:
 	float a,b,c,d; // The equation of plane is ax+by+cz+d=0;
@@ -38,6 +52,22 @@ class plane {
 	}
 };
 
+class projection {
+public:
+	vector<point> vertices;
+	vector<tuple<int, int>> edges;
+}
+
+class model {
+public:
+	vector<vertex> vertices;
+	vector<tuple<int, int>> edges;
+
+	model(vector<vertex> v, vector<tuple<int, int>> e) {
+		vertices = v;
+		edges = e;
+	}
+}
 
 class algorithms {
 	public:
@@ -45,7 +75,7 @@ class algorithms {
 	* Algorithm implementation for converting given 3D model 
 	* into a 2D projection on the given plane.
 	*/
-	vertex algo_3D_to_2D(vertex v, plane p){
+	vertex model_to_projections(vertex v, plane p) {
 
 		float temp1=(get<0>(p.normal)*(get<0>(v.v))+(get<1>(p.normal)*(get<1>(v.v)))+(get<2>(p.normal))*(get<2>(v.v)));
 		float temp2=(p.square_normal());
@@ -53,19 +83,56 @@ class algorithms {
 		vertex projw((temp1/temp2)*(get<0>(p.normal)),(temp1/temp2)*(get<1>(p.normal)),(temp1/temp2)*(get<2>(p.normal)));
 		vertex q((get<0>(v.v)-get<0>(projw.v)),(get<1>(v.v)-get<1>(projw.v)),(get<2>(v.v)-get<2>(projw.v)));
 
-		return q;				
+		return q;
 	}
 
 	/**
 	* Algorithm implementation for converting given 2D projections
 	* into a 2D model.
 	*/
-	void algo_2D_to_3D(){
+	model projections_to_model(projection p1, projection p2, projection p3) {
+		vector<point> v1, v2, v3;
 
+		point first = p1.vertices.at(0);
+		int x1 = get<0>first;
+		int y1 = get<1>first;
+		for (vertex v: p1.vertices) {
+			v1.push_back(make_tuple(get<0>v - x1, get<1>v - y1))
+		}
+
+		point first = p2.vertices.at(0);
+		int x1 = get<0>first;
+		int y1 = get<1>first;
+		for (vertex v: p2.vertices) {
+			v1.push_back(make_tuple(get<0>v - x1, get<1>v - y1))
+		}
+
+		point first = p3.vertices.at(0);
+		int x1 = get<0>first;
+		int y1 = get<1>first;
+		for (vertex v: p3.vertices) {
+			v1.push_back(make_tuple(get<0>v - x1, get<1>v - y1))
+		}
+
+		vector<vertex> vertices;
+		for (int i=0; i<p1.size(); i++) {
+			vertices.push_back(make_tuple(p2.at(i), p3.at(i), p1.at(i)));
+		}
+
+		vector<tuple<int, int>> edges;
+		for (edge e: p1.edges) {
+			if(find(p2.edges.begin(), p2.edges.end(), e) != p2.edges.end()) {
+			    if(find(p3.edges.begin(), p3.edges.end(), e) != p3.edges.end()) {
+			    	edges.push_back(e);
+			    }
+			}
+		}
+
+		return model(vertices, edges);
 	}
 };
 
-int main(){
+int main() {
 
 	vertex v1(2,2,2); //will be a set
 	plane p1(1,2,3,4);
